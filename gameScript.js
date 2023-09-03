f79800ef2051d9d4c4bd7eb5c5dd4f545b0977d4
Edinam_Ukpabio <anietie.godswill@outlook.com>
2023-09-03 23:31:10 +0500

    code added for browsers to start the game

--- gameScript.js
+++ gameScript.js
@@ -19,3 +19,55 @@ function init() {
    $('body').height(H+"px");
    $('#ol').height(H+"px");
 }
+//Showing instructions
+window.onload = function() {
+    $("#ol").html(`<center><div id="inst"><h3>Welcome !</h3>Instructions For Game<br/><br/><li>Make pairs of similiar blocks by flipping them.</li><li>To flip a block you can click on it.</li><li>If two blocks you clicked are not similar, they will be flipped back.</li><p style="font-size:18px;">Click one of the following mode to start the game.</p></div><button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button><button onclick="start(4, 5)">4 x 5</button><button onclick="start(5, 6)">5 x 6</button><button onclick="start(6, 6)">6 x 6</button></center>`);
+}
+
+//Starting the game
+function start(r,l) {
+    //Timer and moves
+    min=0, sec=0, moves=0;
+    $("#time").html("Time: 00:00");
+    $("#moves").html("Moves: 0");
+    time = setInterval(function() {
+      sec++;
+      if(sec==60) {
+          min++; sec=0;
+      }
+      if(sec<10) 
+          $("#time").html("Time: 0"+min+":0"+sec);
+      else 
+        $("#time").html("Time: 0"+min+":"+sec);
+    }, 1000);
+    rem=r*l/2, noItems=rem;
+    mode = r+"x"+l;
+    //Generating item array and shuffling it
+    var items = [];
+    for (var i=0;i<noItems;i++)
+        items.push(em[i]);
+    for (var i=0;i<noItems;i++)
+        items.push(em[i]);
+    var tmp, c, p = items.length;
+    if(p) while(--p) {
+        c = Math.floor(Math.random() * (p + 1));
+        tmp = items[c];
+        items[c] = items[p];
+        items[p] = tmp;
+    }
+    
+    //Creating table
+    $("table").html("");
+    var n=1;
+    for (var i = 1;i<=r;i++) {
+        $("table").append("<tr>");
+        for (var j = 1;j<=l;j++) {
+           $("table").append(`<td id='${n}' onclick="change(${n})"><div class='inner'><div class='front'></div><div class='back'><p>${items[n-1]}</p></div></div></td>`);
+           n++;
+         }
+         $("table").append("</tr>");
+    }
+    
+    //Hiding instructions screen
+    $("#ol").fadeOut(500);
+}
